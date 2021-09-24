176eec3bd34e0bcfbc86b9472fe55a7425f1bc01
maarten descheemaeker <maarten.descheemaeker@sgsintpaulus.eu>
2021-09-24 10:01:30 +0200

    Opdracht Textboxen

---
+++ OpdrTextBoxen/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace OpdrTextBoxen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnLogIn = new System.Windows.Forms.Button();
+            this.btnAnnuleer = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnAnnuleer);
+            this.panel1.Controls.Add(this.btnLogIn);
+            this.panel1.Location = new System.Drawing.Point(235, 122);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(316, 163);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnLogIn
+            // 
+            this.btnLogIn.Location = new System.Drawing.Point(66, 75);
+            this.btnLogIn.Name = "btnLogIn";
+            this.btnLogIn.Size = new System.Drawing.Size(75, 23);
+            this.btnLogIn.TabIndex = 0;
+            this.btnLogIn.Text = "LOG IN";
+            this.btnLogIn.UseVisualStyleBackColor = true;
+            // 
+            // btnAnnuleer
+            // 
+            this.btnAnnuleer.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnAnnuleer.Location = new System.Drawing.Point(226, 75);
+            this.btnAnnuleer.Name = "btnAnnuleer";
+            this.btnAnnuleer.Size = new System.Drawing.Size(75, 23);
+            this.btnAnnuleer.TabIndex = 1;
+            this.btnAnnuleer.Text = "ANNULEER";
+            this.btnAnnuleer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnLogIn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnAnnuleer;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Geef je gegevens in!";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnAnnuleer;
+        private System.Windows.Forms.Button btnLogIn;
+    }
+}
+
