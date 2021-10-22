89bc6ad10d1bc3528392489d67de3b5f4a93c937
maarten descheemaeker <maarten.descheemaeker@sgsintpaulus.eu>
2021-10-22 09:15:41 +0200

    listbox

---
+++ OpdrListBox/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace OpdrListBox
+{
+    partial class frmListBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstItems = new System.Windows.Forms.ListBox();
+            this.txtItem = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstItems
+            // 
+            this.lstItems.FormattingEnabled = true;
+            this.lstItems.Location = new System.Drawing.Point(408, 101);
+            this.lstItems.Name = "lstItems";
+            this.lstItems.Size = new System.Drawing.Size(120, 95);
+            this.lstItems.TabIndex = 0;
+            // 
+            // txtItem
+            // 
+            this.txtItem.Location = new System.Drawing.Point(597, 101);
+            this.txtItem.Name = "txtItem";
+            this.txtItem.Size = new System.Drawing.Size(100, 20);
+            this.txtItem.TabIndex = 1;
+            // 
+            // frmListBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtItem);
+            this.Controls.Add(this.lstItems);
+            this.Name = "frmListBox";
+            this.Text = "ListBox Gebruiken";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstItems;
+        private System.Windows.Forms.TextBox txtItem;
+    }
+}
+
